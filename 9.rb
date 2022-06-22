d95ff23ad6ff24c06ac16e0fa36df8e0ceb1867a
azizdevfull <102867709+azizdevfull@users.noreply.github.com>
2022-06-22 14:02:58 +0500

    Create 9.rb

---
+++ 9.rb
@@ -0,0 +1,12 @@
+def son(p1, p2, p3)
+    if p1 >= 1 && p1 <= 10 || p2 >= 1 && p2 <= 10 || p3 >= 1 && p3 <= 10
+       return true
+    else
+       return false
+
+    end
+    
+ end
+ print son(1, 12, 25),"\n"
+ print son(10, 15, 33),"\n"
+ print son(11, 25, 43),"\n"
