f669ba481f53ea87a86044fa54574bd0ca755646
azizdevfull <102867709+azizdevfull@users.noreply.github.com>
2022-06-22 13:07:17 +0500

    Update 5.rb

--- 5.rb
+++ 5.rb
@@ -1,21 +1,21 @@
 def az(p1,p2)
     if (p1.is_a?(Integer) && p2.is_a?(Integer))
-    if p1 >= 1 && p1 <= 9 && p2 >= 10 && p2 <= 99
+    if p1 >= 0 && p1 <= 9 && p2 >= 10 && p2 <= 99
         "1-chi parametr 1-likda va 2-chi parametr 10-likda kemasligi kerak"
     
-    elsif p1 >= 10 && p1 <= 99 && p2 >= 1 && p2 <= 9
+    elsif p1 >= 10 && p1 <= 99 && p2 >= 0 && p2 <= 9
         "2-chi parametr 1-likda va 1-chi parametr 10-likda kemasligi kerak"
     
-    elsif p1 >= 1 && p1 <= 9 && p2 >= 1 && p2 <= 9
+    elsif p1 >= 0 && p1 <= 9 && p2 >= 0 && p2 <= 9
         "1-chi parametr va 2-chi parametr 10-lik son emas"
     
     elsif p1 >= 10 && p1 <= 99 && p2 >= 10 && p2 <= 99
         return p1 * p2
     end
     
-    else
+    elsif (p1.is_a?(String) && p2.is_a?(String))
         "String kiritish mumkun emas"
     end
     
 end
-puts az(5,55)
+puts az( 10, 10)
